b0c06b37b9937134ca49b4c5b7742396cf9cbee9
Luis <luisegrezende2005@gmail.com>
2025-09-05 19:51:32 -0300

    feito

--- material_radioativo.py
+++ material_radioativo.py
@@ -1,1 +1,14 @@
 # Sua solução aqui
+massa = float(input('Digite a massa inicial do material radioativo (em gramas): '))
+tempo = 0
+
+while massa >= 0.5:
+    massa /= 2
+    tempo += 50
+
+horas = tempo // 3600
+tempo %= 3600
+minutos = tempo // 60
+segundos = tempo % 60
+
+print(f"{horas}h {minutos}m {segundos}s")
